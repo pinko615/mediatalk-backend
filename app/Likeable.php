9d3eec694c423dc4dc2634c07ae84eec28957948
pinko615 <martin.pinto@hotmail.com>
2020-06-10 20:10:16 +0200

    likeable model created

--- app/Likeable.php
+++ app/Likeable.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Likeable extends Model
 {
-    //
+    protected $fillable=[
+        'likeable_id',
+        'likeable_type',
+        'user_id'
+    ];
+
+public function user(){
+    return $this->hasMany('App\User');
+} 
+public function likeable(){
+    return $this->morphTo();
+} 
 }
