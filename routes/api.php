10360b1e06a9746f291c982a9f81611213640593
pinko615 <martin.pinto@hotmail.com>
2020-06-10 20:12:33 +0200

    route config

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('users')->group(function () {
+    Route::post('/register', 'UserController@register');
+    Route::post('/addFollower', 'UserController@createFollower');
+    Route::post('/login', 'UserController@login');
+    Route::get('/{id}', 'UserController@getUserById');
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/logout/{id}', 'UserController@logout');
+        Route::put('/update/{id}', 'UserController@update');
+        Route::post('/updateProfile', 'UserController@editImageProfile');
+    });
 });
+Route::prefix('posts')->group(function () {
+    Route::get('/', 'PostController@show');
+    Route::middleware('auth:api')->group(function () {
+        Route::put('/{id}', 'PostController@update');
+        Route::delete('/{id}', 'PostController@destroy');
+        Route::post('/addLike/{id}', 'LikeableController@addPostLike');
+        Route::post('/addComment/{id}', 'LikeableController@addCommentLike');
+        Route::put('/image/{id}', 'PostController@uploadImage');
+        Route::post('/', 'PostController@create');
+    });
+});
+
+// Add comment to a Post
+Route::post('/addComment', 'CommentController@create');
