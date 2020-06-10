08916df7d5e79159b1608baf01d8fe2131d738a1
pinko615 <martin.pinto@hotmail.com>
2020-06-10 20:09:56 +0200

    post model created

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
+    protected $fillable = [
+        'name',
+        'user_id',
+        'text',
+        'file',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user()
+    {
+       return $this->belongsTo('\App\User');
+    }
+    public function likes()
+    {
+       return $this->morphMany('\App\Likeable','likeable');
+    }
+    public function comments()
+    {
+        return $this->hasMany('\App\Comment');
+    }
 }
